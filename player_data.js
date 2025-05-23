dded463c5606c171c88afb0f979418038ee9c768
WRIRV <gghh70354@gmail.com>
2025-05-24 00:00:40 +0300

    Add files via upload

---
+++ player_data.js
@@ -0,0 +1,186 @@
+const playerData = [
+    {
+        name: 'UnMaded (UNRATED)',
+        veriferData: {
+            name: 'royalGG',
+            url: ''
+        },
+
+        victorsData: [
+            {
+                name: 'Nazorro_',
+                url: 'https://youtu.be/EqH-2yR9TVE'
+            },
+        ]
+    },
+
+    {
+        name: 'uaua factorial',
+        veriferData: {
+            name: 'ExactZ',
+            url: 'https://youtu.be/waFo78I46vg'
+        },
+        
+        victorsData: []
+    },
+
+    {
+        name: 'amethyst challenge',
+        veriferData: {
+            name: 'ItzMiny',
+            url: 'https://drive.google.com/file/d/1kKKranmpiC_ij_tbjdWsPMQOfrfrmFCC/view?usp=sharing'
+        },
+        
+        victorsData: [
+            {
+                name: 'Nazorro_',
+                url: 'https://youtu.be/hsKJNIeewow'
+            },
+        ]
+    },
+
+    {
+        name: 'Boon',
+        veriferData: {
+            name: 'ExactZ',
+            url: ''
+        },
+        
+        victorsData: [
+            {
+                name: 'Nazorro_',
+                url: 'https://youtu.be/bpxCIJuQHf0'
+            },
+
+            {
+                name: 'ItzMiny',
+                url: 'https://drive.google.com/file/d/1_V3RDPktOdGY48jqoRt2xr30UxDlnCKI/view?usp=sharing'
+            },
+        ]
+    },
+
+    {
+        name: 'STEREO BUFFNESS',
+        veriferData: {
+            name: 'Utochka',
+            url: 'https://drive.google.com/file/d/1h2XQk5uGef9-yFFod7WGCVNvENS9fMiC/view?usp=sharing'
+        },
+        
+        victorsData: [
+            {
+                name: 'Nazorro_',
+                url: 'https://youtu.be/0ag0FUi_-6M'
+            },
+        ]
+    },
+
+    {
+        name: 'untitled unnamed',
+        veriferData: {
+            name: 'ExactZ',
+            url: 'https://drive.google.com/file/d/1AsbJLBpJ4g1Lo3kGOSfOxrhP6sgoFRzN/view?usp=sharing'
+        },
+        
+        victorsData: [
+            {
+                name: 'Nazorro_',
+                url: 'https://youtu.be/u6KL3hgp4ik'
+            },
+        ]
+    },
+
+    {
+        name: 'The Egoist',
+        veriferData: {
+            name: 'Setiop',
+            url: ''
+        },
+        
+        victorsData: []
+    },
+
+    {
+        name: 'green wave buff (UNRATED)',
+        veriferData: {
+            name: 'ExactZ',
+            url: 'https://drive.google.com/file/d/1-Vh840fLEjSLq1-6vJg2ArKCicp7jPuQ/view?usp=sharing'
+        },
+        
+        victorsData: []
+    },
+
+    {
+        name: 'Clutter God',
+        veriferData: {
+            name: 'ExactZ',
+            url: ''
+        },
+        
+        victorsData: [
+            {
+                name: 'ItzMiny',
+                url: 'https://drive.google.com/file/d/10ZkBzlgHc52kiVmE-bqrvZLMvNUV2zYI/view?usp=sharing'
+            },
+        ]
+    },
+
+    {
+        name: 'Green Wave',
+        veriferData: {
+            name: 'ferx1n56',
+            url: ''
+        },
+        
+        victorsData: []
+    },
+
+    {
+        name: 'Top 1',
+        veriferData: {
+            name: 'ferx1n56',
+            url: ''
+        },
+        
+        victorsData: []
+    },
+
+    {
+        name: 'beZ doZi',
+        veriferData: {
+            name: 'Beist',
+            url: ''
+        },
+        
+        victorsData: [
+            {
+                name: 'Utochka',
+                url: 'https://drive.google.com/file/d/1D7IddgqMNrpm4cBmUclwhD3vf7S_6hPX/view?usp=sharing'
+            },
+
+            {
+                name: 'CsGoDimas30',
+                url: 'https://youtu.be/cGrINQTWrmc'
+            },
+        ]
+    },
+
+    {
+        name: 'The Prodigy',
+        veriferData: {
+            name: 'Setiop',
+            url: ''
+        },
+        
+        victorsData: []
+    },
+
+    {
+        name: 'Undecored Chaos',
+        veriferData: {
+            name: 'Setiop',
+            url: ''
+        },
+        
+        victorsData: []
+    },
+];
